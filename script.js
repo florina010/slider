a48ee4e6cb7f8ef2892c41961d43abcc46025cc5
Florina Dascal <dascal_florina@yahoo.com>
2017-08-22 11:03:49 +0300

    each in script

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 "use strict";
-$(document).ready( function () {
-      $('#listaMea').slider({
-        animate : "true",
-        transitionTime: "2s",
-        displayPrevNext : "true",
-        infinitTransition : "false",
-        slideEl: "div",
-        vertical: "false"
-      });
-      $('#listaTa').slider({
-        animate : "false",
-        transitionTime: "2s",
-        displayPrevNext : "true",
-        infinitTransition : "false",
-        slideEl: "li",
-        vertical: "true"
-      });
+$(document).ready ( function () {
+  $(".listaMea").each(function () {
+    $(this).slider({
+      animate : "true",
+      transitionTime : "2s",
+      displayPrevNext : "true",
+      infinitTransition : "false",
+      slideEl: "div",
+      vertical : "false"
+    });
+  });
 });
--- slider.js
+++ slider.js
@@ -8,61 +8,27 @@
         infinitTransition : "true",
         slideEl : "li"
     }, obj );
-
-  var slides , length, id = 0, sliderId, spans, infinite, divH, sliderObj;
-
-  function changeSlides (id) {
-    var nextId, previousId;
-    nextId = id === length - 1 ? 0 : id + 1;
-    previousId = id === 0 ? length - 1 : id - 1;
-
-    if ( options['displayPrevNext'] == 'false'){
-        $(slides[previousId]).children().css("display", "none");
-        $(slides[nextId]).children().css("display", "none");
-      }
-    if (options['infinitTransition'] == 'false'){
-      if ( id == length - 1 )
-        clearInterval(infinite);
-    }
-
-    for (var i = 0; i < length; i++) {
-       if ( options['animate'] == "true" )
-         $(slides[i]).children().css({"animation" : "animate  " + options["transitionTime"]});
-      if (i === id) {
-        $(slides[nextId]).children().removeClass($(slides[nextId]).children().attr("class")).addClass('right');
-        $(slides[previousId]).children().removeClass($(slides[previousId]).children().attr("class")).addClass('left');
-        $(slides[i]).children().removeClass($(slides[i]).children().attr("class")).addClass('active');
-      }
-      if (i != nextId && i != previousId && i != id){
-        $(slides[i]).children().removeClass($(slides[i]).children().attr("class")).addClass('inactive');
-      }
-    }
-  };
+  var sliderOb =this, slides , length, id = 0, infinite;
 
     function vertical () {
-        $("." + sliderId).css({"display" : "table-caption ", "margin-left" : "0px"}  );
-        $("#" + sliderId).css({"position" : "relative", "bottom" : "20px", "right" : "0px", "left" : "0px", "height" : "10px", "margin-bottom" : "250px"});
-        $("#" + sliderId + " .active").css({"left" : "87px", "top" : "230px", "clip" : "auto"});
-        $("#" + sliderId + " .left").css({"left" : "87px", "top" : "0px", "clip" : "rect(50px, 866px, 200px, 0px)", "width" : "41%" });
-        $("#" + sliderId + " .right").css({"left" : "87px", "top" : "410px", "clip" : "rect(50px, 866px, 200px, 0px)", "width" : "41%", "margin-bottom" : "200px" });
+      $(sliderOb.children().last()).css({"display" : "table-caption ", "margin-left" : "0px"} );
+      $(slides).children().each( function () {
+        if ($(this).attr("class") == 'left')
+          $(this).css({"left" : "87px", "top" : "0px", "clip" : "rect(50px, 866px, 200px, 0px)", "width" : "41%" });
+        if ($(this).attr("class") == 'right')
+          $(this).css({"left" : "87px", "top" : "410px", "clip" : "rect(50px, 866px, 200px, 0px)", "width" : "41%", "margin-bottom" : "200px" });
+        if ($(this).attr("class") == 'active')
+          $(this).css({"left" : "87px", "top" : "230px", "clip" : "auto"});
+      });
+        $(sliderOb).css({"position" : "relative", "bottom" : "20px", "right" : "0px", "left" : "0px", "height" : "10px", "margin-bottom" : "600px"});
 
-/*
-      if ( options["slideEl"] == "li" ){
-        $("." + sliderId).css("display" , "table-caption ");
-        $("." + sliderId).children().css("margin-top" , "50px");
-        $("#" + sliderId).css({"margin-top" : "20px", "margin-bottom" : "75px"});
-        $("#" + sliderId + " .active").css({"left" : "87px", "top" : "230px", "clip" : "auto",  "position" : "absolute"});
-        $("#" + sliderId +  " .left").css({"left" : "87px", "top" : "0px", "clip" : "rect(50px, 866px, 200px, 0px)", "width" : "41%",  "position" : "absolute" });
-        $("#" + sliderId + " .right").css({"left" : "87px", "top" : "410px", "clip" : "rect(50px, 866px, 200px, 0px)", "width" : "41%",  'position' : "absolute" });
-        $("div:first").css("margin-bottom", "200px");
-      }*/
     };
 
     function start () {
       infinite = setInterval( function () {
          if (id == length)
            id = 0;
-        $(".".concat(sliderId).concat(" span")).each( function () {
+        spans.each( function () {
           if ( $(this).attr("id") != parseInt(id) )
             $(this).css("background-color", "#fff");
           else
@@ -76,43 +42,75 @@
   };
 
   (function generateButtons (sliderObj) {
-    sliderId = sliderObj.attr("id");
     var span= $("<span></span>");
-    slides = sliderObj.find(options.slideEl);
-    length = slides.length;
-    var ndiv = $('<div></div>');
-    var pager = $('<div class="pager"></div>');
-    pager.addClass(sliderId);
+    sliderObj.each( function () {
+      slides = $(this).find(options.slideEl);
+      length = slides.length;
+      var pager = $('<div class="pager"></div>');
 
-    for (var i = 0; i < length; i++){
-      var divSpan = $("<div></div>"), span = $("<span></span>");
-      span.attr("id", i);
-      $(divSpan).append(span);
-      if (i == 0)
-        span.css("background-color", "#000");
-      else
-        span.css("background-color", "#fff");
-      pager.append(divSpan);
-
-      span.each( function () {
-        $(this).click( function () {
-          $(this).css("background-color", "#000");
-          $(".".concat(sliderId).concat(" span")).not($(this)).css("background-color", "#fff");
-          id = parseInt($(this).attr('id'));
-          changeSlides(id);
-          if (options['vertical'] == "true")
-            vertical();
-          start();
-        });
-      });
+      for (var i = 0; i < length; i++){
+        var divSpan = $("<div></div>"), span = $("<span></span>");
+        span.attr("id", i);
+        $(divSpan).append(span);
+        if (i == 0)
+          span.css("background-color", "#000");
+        else
+          span.css("background-color", "#fff");
+        pager.append(divSpan);
     }
-
-    ndiv.append(pager);
-    ndiv.insertAfter(sliderObj);
+    if ( options["vertical"] == "true"){
+      vertical();
+      sliderObj.append(pager);
+    }
+    else
+      sliderObj.append(pager);
     start();
+    });
 
   })($(this));
 
-    return this;
+  var spans = $(sliderOb.children().last().children().children());
+
+  spans.each( function () {
+    $(this).click( function () {
+      $(this).css("background-color", "#000");
+      spans.not($(this)).css("background-color", "#fff");
+      id = parseInt($(this).attr('id'));
+      changeSlides(id);
+      if (options['vertical'] == "true")
+        vertical();
+      start();
+    });
+  });
+
+  function changeSlides (id) {
+    var nextId, previousId, image;
+    nextId = id === length - 1 ? 0 : id + 1;
+    previousId = id === 0 ? length - 1 : id - 1;
+    image = sliderOb.children().children();
+
+    if ( options['displayPrevNext'] == 'false'){
+        $(image[previousId]).css("display", "none");
+        $(image[nextId]).css("display", "none");
+      }
+    if (options['infinitTransition'] == 'false'){
+      if ( id == length - 1 )
+        clearInterval(infinite);
+    }
+    for (var i = 0; i < length; i++) {
+       if ( options['animate'] == "true" )
+         $(image[i]).css({"animation" : "animate  " + options["transitionTime"]});
+      if (i === id) {
+        $(image[nextId]).removeClass($(image[nextId]).attr("class")).addClass('right');
+        $(image[previousId]).removeClass($(image[previousId]).attr("class")).addClass('left');
+        $(image[i]).removeClass($(image[i]).attr("class")).addClass('active');
+      }
+      if (i != nextId && i != previousId && i != id){
+        $(image[i]).removeClass($(image[i]).attr("class")).addClass('inactive');
+      }
+    }
+  };
+
+      return this;
   }
 }( jQuery ));
